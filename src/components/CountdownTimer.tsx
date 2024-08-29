1d1039ea078863c2b592ae110ac225ed97faa21a
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-08-29 16:18:59 +0200

    Done

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -2,42 +2,59 @@ import { useState, useEffect, useRef } from "react";
 import "./CountdownTimer.css"
 
 export function CountdownTimer(){
-    const [timeLeft, setTimeLeft] = useState<number>(4);
-    const [isActive, setIsActive] = useState(true);
-    const timerRef = useRef();
-    const timerRef = useRef();
-
+    const initialTime = 4; 
+    const [timeLeft, setTimeLeft] = useState<number>(initialTime);
+    const [isActive, setIsActive] = useState(false);
+    const intervalID = useRef<number | null>(null);
+    
     useEffect(() => {
-        if(isActive){
+
+        console.log("Just started useEffect ", intervalID, isActive)
+        // if(timeLeft <= 0){
+        //     console.log("Inside timeLeft <= 0 ", intervalID);
+        //     clearInterval(intervalID.current as number);
+
+        // }
+        
+        if(isActive && timeLeft > 0){
+            intervalID.current = setInterval(() => {
+                if(timeLeft > 0){
+                    setTimeLeft((t) => (t - 1))
+                    setIsActive(true);
+                }else{
+                    setIsActive(false);
+                }  
+            }, 1000)
+            
+        }else if(timeLeft === 0){
             setIsActive(false);
-        };
+            console.log("Not active", intervalID.current as number);
+        }
+        
+        return () => {
+            if(intervalID.current){
+                clearInterval(intervalID.current)
+            }
+        }
 
-
-    }, [timeLeft, isActive, ]);
-
-       
-   
-
+    }, [isActive, timeLeft]);
     
- 
     
-    function handleStart(){
-        setInterval(()=> {
-            while(timeLeft >= 0 && isActive){
-                setTimeLeft((timeLeft) => (timeLeft - 1))
-            };
-        }, 1000);
+
+    function handleReset(){
+        setTimeLeft(initialTime);
+        setIsActive(false);
+        
     }
-
 
     return (
         <>
                 <h1>Nedräkningstimer</h1>
-                <h2 ref={timerRef}>{timeLeft} sekunder kvar</h2>
+                <h2>{timeLeft} sekunder kvar</h2>
                 <div className="buttonContainer">
-                    <button ref={}>Starta</button>
-                    <button>Pausa</button>
-                    <button>Återställ</button>
+                    <button onClick={() => setIsActive(true)}>Starta</button>
+                    <button onClick={() => setIsActive(false)}>Pausa</button>
+                    <button onClick={handleReset}>Återställ</button>
                 </div>
         </>
     );
