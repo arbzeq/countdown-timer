1cad3d705950b33bdbe4628c1aef0026d81bdb7f
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-08-29 10:39:21 +0200

    k

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -2,24 +2,40 @@ import { useState, useEffect, useRef } from "react";
 import "./CountdownTimer.css"
 
 export function CountdownTimer(){
-    const [timeLeft, setTimeLeft] = useState(4);
+    const [timeLeft, setTimeLeft] = useState<number>(4);
     const [isActive, setIsActive] = useState(true);
+    const timerRef = useRef();
+    const timerRef = useRef();
 
-    useEffect({} = > ()
-
+    useEffect(() => {
         if(isActive){
             setIsActive(false);
-        }:
+        };
+
+
+    }, [timeLeft, isActive, ]);
+
+       
+   
+
+    
  
-    ))
+    
+    function handleStart(){
+        setInterval(()=> {
+            while(timeLeft >= 0 && isActive){
+                setTimeLeft((timeLeft) => (timeLeft - 1))
+            };
+        }, 1000);
+    }
 
 
     return (
         <>
                 <h1>Nedräkningstimer</h1>
-                <h2>{timeLeft} sekunder kvar</h2>
+                <h2 ref={timerRef}>{timeLeft} sekunder kvar</h2>
                 <div className="buttonContainer">
-                    <button>Starta</button>
+                    <button ref={}>Starta</button>
                     <button>Pausa</button>
                     <button>Återställ</button>
                 </div>
